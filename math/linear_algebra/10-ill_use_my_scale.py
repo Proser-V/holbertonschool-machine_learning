651974bbc49c37f12449753904b2e44b70a900ce
Proser-V <10530@holbertonstudents.com00>
2025-12-04 22:04:46 +0100

    10-ill_use_my_scale.py

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+A module that contains a function that calculates the shape of a numpy.ndarray.
+"""
+
+
+def np_shape(matrix):
+    """
+    The function that calculates the shape of a numpy.ndarray.
+    """
+    return matrix.shape
