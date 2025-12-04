8e3adfb773379cb85d96f1a9249d5f0f694f5dc0
Proser-V <10530@holbertonstudents.com00>
2025-12-04 23:50:52 +0100

    101-the_whole_barn.py

---
+++ math/linear_algebra/101-the_whole_barn.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+A module that contains a function that adds two matrices element-wise.
+"""
+
+
+def add_matrices(mat1, mat2):
+    """Adds two matrices element-wise.
+    """
+
+    if type(mat1) != type(mat2):
+        return None
+    if not isinstance(mat1, list):
+        return mat1 + mat2
+    if len(mat1) != len(mat2):
+        return None
+
+    result = []
+    for a, b in zip(mat1, mat2):
+        summed = add_matrices(a, b)
+        if summed is None:
+            return None
+        result.append(summed)
+
+    return result
