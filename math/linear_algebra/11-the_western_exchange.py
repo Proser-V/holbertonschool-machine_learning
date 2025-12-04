073cdc372f299da88ebc6f00837aff6753502e42
Proser-V <10530@holbertonstudents.com00>
2025-12-04 23:12:28 +0100

    11-the_western_exchange.py

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+A module that contains a function that transposes a matrix.
+"""
+
+
+def np_transpose(matrix):
+    """
+    The function that transposes a matrix.
+    """
+    return matrix.T
