979ae5fa4c3ce68a48888f49c4248fed766f69b2
Proser-V <10530@holbertonstudents.com00>
2025-12-04 16:24:47 +0100

    2-size_me_please.py

---
+++ math/linear_algebra/2-size_me_please.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+A module that contains the function to calculate the shape of a matrix.
+"""
+
+
+def matrix_shape(matrix):
+    """
+    A function that calculate the shape of a matrix.
+    """
+    shape = []
+    while isinstance(matrix, list):
+        shape.append(len(matrix))
+        matrix = matrix[0]
+    return shape
