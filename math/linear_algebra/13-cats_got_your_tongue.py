16be47d128324cb0f567447bb48801a7fd02a581
Proser-V <10530@holbertonstudents.com00>
2025-12-04 23:25:22 +0100

    13-cats_got_your_tongue.py

---
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+A module that contains a function that concatenates two matrices along a specific axis.
+"""
+
+
+import numpy as np
+
+def np_cat(mat1, mat2, axis=0):
+    """
+    The function that concatenates two matrices along a specific axis.
+    """
+    return np.concatenate([mat1, mat2], axis)
