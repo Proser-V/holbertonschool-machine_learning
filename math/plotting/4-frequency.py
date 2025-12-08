8f41bded4b455695edd16e2c36f13d5006cbbc9f
Proser-V <10530@holbertonstudents.com00>
2025-12-08 21:32:55 +0100

    4-frequency.py

---
+++ math/plotting/4-frequency.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+A module that contains a function that plots a histogram.
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def frequency():
+    """
+    The function that plots a histogram.
+    """
+    np.random.seed(5)
+    student_grades = np.random.normal(68, 15, 50)
+    plt.figure(figsize=(6.4, 4.8))
+
+    plt.hist(student_grades, bins=10, edgecolor="black")
+    plt.title("Project A")
+
+    plt.xlabel("Grades")
+    plt.ylabel("Number of Students")
+
+    plt.show()
