54582bdf0da20f4e5eeb498d1a5a8f52b2367ac9
Proser-V <10530@holbertonstudents.com00>
2025-12-08 21:47:55 +0100

    4-frequency.py

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -15,7 +15,7 @@ def frequency():
     plt.figure(figsize=(6.4, 4.8))
 
     bins = np.arange(0, 101, 10)
-    plt.hist(student_grades, bins=bins, color="blue", edgecolor="black")
+    plt.hist(student_grades, bins=bins, edgecolor="black")
     plt.title("Project A")
 
     plt.xlabel("Grades")
