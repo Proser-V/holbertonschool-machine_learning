a23d2399f9dcaa35d14a9ad58ff9fa3e556260e4
Proser-V <10530@holbertonstudents.com00>
2025-12-08 14:53:31 +0100

    0-line.py pycodestyle and documentation

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
+"""
+A module that contains a function that plots a line graph.
+"""
 import numpy as np
 import matplotlib.pyplot as plt
 
-def line():
 
+def line():
+    """
+    The function that plots a line graph.
+    """
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
 
